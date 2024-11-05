32156168c0f197f0a42c9102864109cd04f45f35
Sankar <134912048+sankar0812@users.noreply.github.com>
2024-11-05 11:05:30 +0530

    add$

--- src/components/Route/ProductCard/ProductCard.jsx
+++ src/components/Route/ProductCard/ProductCard.jsx
@@ -64,17 +64,17 @@ const ProductCard = ({ data,isEvent }) => {
     <>
       <div className="w-full h-[370px] bg-white rounded-lg shadow-sm p-3 relative cursor-pointer">
         <div className="flex justify-end"></div>
-        <Link to={`₹{isEvent === true ? `/product/₹{data._id}?isEvent=true` : `/product/₹{data._id}`}`}>
+        <Link to={`${isEvent === true ? `/product/${data._id}?isEvent=true` : `/product/${data._id}`}`}>
           <img
-            src={`₹{data.images && data.images[0]?.url}`}
+            src={`${data.images && data.images[0]?.url}`}
             alt=""
             className="w-full h-[170px] object-contain"
           />
         </Link>
-        <Link to={`/shop/preview/₹{data?.shop._id}`}>
-          <h5 className={`₹{styles.shop_name}`}>{data.shop.name}</h5>
+        <Link to={`/shop/preview/${data?.shop._id}`}>
+          <h5 className={`${styles.shop_name}`}>{data.shop.name}</h5>
         </Link>
-        <Link to={`₹{isEvent === true ? `/product/₹{data._id}?isEvent=true` : `/product/₹{data._id}`}`}>
+        <Link to={`${isEvent === true ? `/product/${data._id}?isEvent=true` : `/product/${data._id}`}`}>
           <h4 className="pb-3 font-[500]">
             {data.name.length > 40 ? data.name.slice(0, 40) + "..." : data.name}
           </h4>
@@ -85,14 +85,14 @@ const ProductCard = ({ data,isEvent }) => {
 
           <div className="py-2 flex items-center justify-between">
             <div className="flex">
-              <h5 className={`₹{styles.productDiscountPrice}`}>
+              <h5 className={`${styles.productDiscountPrice}`}>
                 {data.originalPrice === 0
                   ? data.originalPrice
                   : data.discountPrice}
-                ₹
+                $
               </h5>
-              <h4 className={`₹{styles.price}`}>
-                {data.originalPrice ? data.originalPrice + " ₹" : null}
+              <h4 className={`${styles.price}`}>
+                {data.originalPrice ? data.originalPrice + " $" : null}
               </h4>
             </div>
             <span className="font-[400] text-[17px] text-[#68d284]">
